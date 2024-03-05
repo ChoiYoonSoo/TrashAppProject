73e7ba23652392852504d6639ca1731c3f28b66b
ChoiYoonSoo <gews30025@naver.com>
2024-03-06 01:19:15 +0900

    issue - 로그인 화면 구현 및 폰트 설정

--- app/src/main/java/com/example/trashapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/trashapp/view/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.trashapp.view.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.trashapp.R
 
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         val startWithoutLoginButton = findViewById<TextView>(R.id.startWithoutLogin)
         startWithoutLoginButton.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginButton = findViewById<Button>(R.id.btnLoginSignUp)
+        loginButton.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
