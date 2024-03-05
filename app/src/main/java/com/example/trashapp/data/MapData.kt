d19794876cdda0481856bb7ecd0c6562b63055a1
ChoiYoonSoo <gews30025@naver.com>
2024-03-05 17:11:07 +0900

    issue - 0305 프로젝트 구조 조정 및 메인 UI 수정

---
+++ app/src/main/java/com/example/trashapp/data/MapData.kt
@@ -0,0 +1,7 @@
+package com.example.trashapp.data
+
+data class MapData(
+    var name: String? = null,
+    var latitude : Double,  // 위도
+    var longitude : Double, // 경도
+)
--- app/src/main/java/com/example/trashapp/view/activities/SplashActivity.kt
+++ app/src/main/java/com/example/trashapp/view/activities/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.trashapp
+package com.example.trashapp.view.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
