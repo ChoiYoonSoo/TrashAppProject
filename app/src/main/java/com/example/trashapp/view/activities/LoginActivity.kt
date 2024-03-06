31dc3f33088af2456311a548b613c72bd00320a4
ChoiYoonSoo <gews30025@naver.com>
2024-03-06 14:50:01 +0900

    issue - 로그인 화면 뒤로가기 버튼 구현

--- app/src/main/java/com/example/trashapp/view/activities/LoginActivity.kt
+++ app/src/main/java/com/example/trashapp/view/activities/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.trashapp.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import com.example.trashapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener{
+           finish()
+        }
     }
 }
