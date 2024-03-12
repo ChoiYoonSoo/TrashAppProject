5081c0429be5e0ae883df965470f6dd57910241e
ChoiYoonSoo <gews30025@naver.com>
2024-03-12 15:54:00 +0900

    issue - 회원가입 화면 구현

--- app/src/main/java/com/example/trashapp/view/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/trashapp/view/fragments/SignUpFragment.kt
@@ -5,37 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import com.example.trashapp.R
-import com.example.trashapp.databinding.FragmentLoginBinding
+import com.example.trashapp.databinding.FragmentSignUpBinding
 
-class LoginFragment : Fragment() {
-
-    private lateinit var binding : FragmentLoginBinding
-
+class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentSignUpBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.backButton.setOnClickListener{
             parentFragmentManager.popBackStack()
-        }
-
-        binding.btnSignUp.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_signUpFragment)
         }
     }
 
