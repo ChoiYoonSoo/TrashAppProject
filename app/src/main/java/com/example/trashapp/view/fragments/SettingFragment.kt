5c6f8647a483b293b4f858e0aafd013220ba36b2
ChoiYoonSoo <gews30025@naver.com>
2024-03-24 02:34:34 +0900

    issue - 1차 피그마 UI 구현 완료

--- app/src/main/java/com/example/trashapp/view/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/trashapp/view/fragments/SettingFragment.kt
@@ -1,10 +1,14 @@
 package com.example.trashapp.view.fragments
 
+import android.graphics.Outline
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import android.widget.ImageView
 import androidx.navigation.Navigation
 import com.example.trashapp.R
 import com.example.trashapp.databinding.FragmentSettingBinding
@@ -29,6 +33,18 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // 프로필 이미지 원형 변경
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val imageView = view.findViewById<ImageView>(R.id.profIleImage)
+            imageView.outlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View?, outline: Outline?) {
+                    // ImageView의 크기에 맞는 원형 outline 설정
+                    outline?.setOval(0, 0, view!!.width, view.height)
+                }
+            }
+            imageView.clipToOutline = true
+        }
+
         binding.settingBackButton.setOnClickListener{
             parentFragmentManager.popBackStack()
         }
@@ -43,6 +59,10 @@ class SettingFragment : Fragment() {
 
         binding.contactButton.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_settingFragment_to_contactFragment)
+        }
+
+        binding.unSubButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_settingFragment_to_unsubFragment)
         }
     }
 
--- app/src/main/java/com/example/trashapp/view/fragments/UnsubFragment.kt
+++ app/src/main/java/com/example/trashapp/view/fragments/UnsubFragment.kt
@@ -1,12 +1,19 @@
 package com.example.trashapp.view.fragments
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.trashapp.R
+import com.example.trashapp.databinding.FragmentUnsubBinding
+
 class UnsubFragment : Fragment() {
+
+    private lateinit var binding : FragmentUnsubBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,27 @@ class UnsubFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unsub, container, false)
+        binding = FragmentUnsubBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.unSubBackButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        binding.unSub.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.unsub_dialog)
+            dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            val button : Button = dialog.findViewById(R.id.unsubConfirm)
+            button.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+
+        }
     }
 }
